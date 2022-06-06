be91902368f1bcf74c8d49ed1cf0b266dd930b1c
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-06-06 13:11:23 +0400

    added modal and overlay

--- js/index.js
+++ js/index.js
@@ -1,3 +1,60 @@
+const createElem = (tag, attr) => {
+  const elem = document.createElement(tag);
+
+  return Object.assign(elem, { ...attr });
+};
+
+const createModal = (title, description) => {
+  const overlayElem = createElem('div', { className: 'modal' });
+  const modalElem = createElem('div', { className: 'modal__block' });
+  const modalContainerElem = createElem('div', { className: 'modal__container' });
+
+  const titleElem = createElem('h2', { 
+    className: 'modal__title',
+    textContent: `Заказать ${title}` });
+
+    modalContainerElem.append(titleElem)
+    modalElem.append(modalContainerElem);
+
+  overlayElem.append(modalElem);
+  return overlayElem;
+}
+
+const productTitle = document.querySelectorAll('.product__title');
+const productDescription = document.querySelectorAll('.product__description');
+const productBtn = document.querySelectorAll('.product__btn');
+
+for (let i = 0; i < productBtn.length; i++) {
+  productBtn[i].addEventListener('click', () => {
+    const title = productTitle[i].textContent;
+    const description = productDescription[i].textContent;
+
+    const modal = createModal(title, description);
+
+    document.body.append(modal);
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // yandex map
 
 // const init = () => {
@@ -35,3 +92,4 @@ L.marker([55.7724, 37.6252])
   .addTo(map)
   .bindPopup('E-trans')
   .openPopup();
+
