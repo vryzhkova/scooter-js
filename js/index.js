8a1764e0485694caeecc518cbafce2317c15158d
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-06-07 15:17:36 +0400

    added lesson 4

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+const disabledScroll = () => {
+  document.body.scrollPosition = window.scrollY;
+  document.body.style.cssText = `
+    overflow: hidden;
+    position: fixed;
+    top: -${document.body.scrollPosition}px;
+    left: 0;
+    height: 100wh;
+    width: 100wv;
+    padding-right: ${window.innerWidth - document.body.offsetWidth}px;
+  `;
+}
+
+const enabledScroll = () => {
+  document.body.style.cssText = '';
+  window.scroll({top: document.body.scrollPosition})
+};
+
 const createElem = (tag, attr) => {
   const elem = document.createElement(tag);
 
@@ -13,11 +31,84 @@ const createModal = (title, description) => {
     className: 'modal__title',
     textContent: `Заказать ${title}` });
 
-    modalContainerElem.append(titleElem)
-    modalElem.append(modalContainerElem);
+  const descriptionElem = createElem('p', { 
+    className: 'modal__description',
+    textContent: description });
 
+  const formElem = createElem('form', { 
+      className: 'modal__form',
+      method: 'post',
+      action: 'https://jsonplaceholder.typicode.com/posts',
+      id: 'order',
+    });
+
+  const nameLabelElem = createElem('label', { 
+    className: 'modal__label', 
+  });
+
+  const nameSpanElem = createElem('span', { 
+    className: 'modal__text',
+    textContent: 'Имя',
+  });
+
+  const nameInputElem = createElem('input', { 
+    className: 'modal__input',
+    placeholder: 'Введите ваше имя',
+    name: 'name',
+    required: true,
+  });
+
+  const phoneLabelElem = createElem('label', { className: 'modal__label' });
+  const phoneSpanElem = createElem('span', { 
+    className: 'modal__text',
+    textContent: 'Телефон',
+   });
+
+  const phoneInputElem = createElem('input', { 
+    className: 'modal__input',
+    placeholder: 'Введите ваш телефон',
+    name: 'phone',
+    required: true,
+  });
+
+  const hideInput = createElem('input', {
+    type: 'hidden',
+    name: 'product',
+    value: title,
+  });
+
+  const btnSubmit = createElem('button', {
+    className: 'modal__btn',
+    textContent: 'Заказать',
+    type: 'submit',
+  });
+  btnSubmit.setAttribute('form', 'order');
+
+  const closeModalBtn = createElem('button', {
+    className: 'modal__close',
+    innerHTML: `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M17.75 2.0125L15.9875 0.25L9 7.2375L2.0125 0.25L0.25 2.0125L7.2375 9L0.25 15.9875L2.0125 17.75L9 10.7625L15.9875 17.75L17.75 15.9875L10.7625 9L17.75 2.0125Z" fill="#18171A"/>
+    </svg>
+    `,
+  });
+
+  overlayElem.addEventListener('click', event => {
+    const target = event.target;
+    if(target === overlayElem || target.closest('.modal__close')) {
+      overlayElem.remove();
+      enabledScroll();
+    }
+  })
+
+  nameLabelElem.append(nameSpanElem, nameInputElem);
+  phoneLabelElem.append(phoneSpanElem, phoneInputElem);
+  formElem.append(nameLabelElem, phoneLabelElem, hideInput);
+
+  modalContainerElem.append(titleElem, descriptionElem, formElem, btnSubmit, closeModalBtn);
+  modalElem.append(modalContainerElem);
   overlayElem.append(modalElem);
-  return overlayElem;
+  disabledScroll();
+  document.body.append(overlayElem);
 }
 
 const productTitle = document.querySelectorAll('.product__title');
@@ -28,32 +119,9 @@ for (let i = 0; i < productBtn.length; i++) {
   productBtn[i].addEventListener('click', () => {
     const title = productTitle[i].textContent;
     const description = productDescription[i].textContent;
-
-    const modal = createModal(title, description);
-
-    document.body.append(modal);
+    createModal(title, description);
   })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // yandex map
 
